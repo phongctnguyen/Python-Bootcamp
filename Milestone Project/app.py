84f0e4df2ff5785f7ab63b84f6a43a1df1660eb5
phongctnguyen <phongctnguyen@gmail.com>
2020-09-03 21:45:41 +0700

    finish milestone project and start oop section

---
+++ Milestone Project/app.py
@@ -0,0 +1,80 @@
+"""
+Enter 'a' to add a movie
+Enter 'l' to see a movie
+Enter 'f' to find a movie
+Enter 'q' to quit
+
+Tasks:
+- Decide where to store movies
+- What is the format of a movie?
+- Show the user the main interface and get their input
+- Allow user to add movies
+- Show all their movies
+- Find a movie
+- Stop running the program when they type 'q'
+"""
+
+movies = []
+
+
+def menu():
+    user_input = input(
+        "Enter 'a' to add a movie, 'l' to see a movie, 'f' to find a movie, 'q' to quit: ")
+
+    while user_input != 'q':
+        if user_input == 'a':
+            add_movie()
+        elif user_input == 'l':
+            show_movies(movies)
+        elif user_input == 'f':
+            find_movie()
+        else:
+            print('Unknown command, please try again')
+
+        user_input = input(
+            "\nEnter 'a' to add a movie, 'l' to see a movie, 'f' to find a movie, 'q' to quit: ")
+
+
+def add_movie():
+    name = input('Enter the movie name: ')
+    director = input('Enter the movie director: ')
+    year = input('Enter the movie release year: ')
+
+    movies.append({
+        'name': name,
+        'director': director,
+        'year': year
+    })
+
+
+def show_movies(movie_list):
+    for movie in movie_list:
+        show_movie_details(movie)
+
+
+def show_movie_details(movie):
+    print(f"Name: {movie['name']}")
+    print(f"Director: {movie['director']}")
+    print(f"Year: {movie['year']}")
+
+
+def find_movie():
+    find_by = input('What property of the movie are you looking for? ')
+    looking_for = input('What are you searching for? ')
+
+    found_movies = find_by_attribute(movies, looking_for, lambda x: x[find_by])
+
+    show_movies(found_movies)
+
+
+def find_by_attribute(items, expected, finder):
+    found = []
+
+    for i in items:
+        if finder(i) == expected:
+            found.append(i)
+
+    return found
+
+
+menu()
---
+++ OOP/class.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, new_name, new_grades):
+        self.name = new_name
+        self.grades = new_grades
+
+    def average(self):
+        return sum(self.grades) / len(self.grades)
+
+
+student_one = Student('Rolf Smith', [70, 88, 90, 99])
+print(student_one.average())
